25c9929ea73b6ba14eeed7d7f3ad935a80951eea
HaoChen <b08901013@ntu.edu.tw>
2022-10-27 13:19:26 +0800

    create modules

---
+++ aud_player.v
@@ -0,0 +1,11 @@
+module AudPlayer(
+	input i_rst_n,
+	input i_bclk,
+	input i_daclrck,
+	input i_en, // enable AudPlayer only when playing audio, work with AudDSP
+	input i_dac_data, //dac_data
+	output o_aud_dacdat
+);
+
+
+endmodule
---
+++ aud_recorder.v
@@ -0,0 +1,14 @@
+module AudRecorder(
+	input i_rst_n, 
+	input i_clk,
+	input i_lrc,
+	input i_start,
+	input i_pause,
+	input i_stop,
+	input i_data,
+	output o_address,
+	output o_data,
+);
+
+
+endmodule
