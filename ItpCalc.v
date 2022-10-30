33f00b1ec17d08c1f6f04f2c4a42c457211521fb
Tyan-0978 <960007nathan@gmail.com>
2022-10-31 01:20:06 +0800

    initialize interpolation calculation module

---
+++ ItpCalc.v
@@ -0,0 +1,80 @@
+// ----------------------------------------------------------------------
+// interpolation calculation module
+// ----------------------------------------------------------------------
+
+module itp2 (
+    input  [15:0] i_data_1,
+    input  [15:0] i_data_2,
+    output [15:0] o_data,
+);
+
+endmodule
+
+module itp3 (
+    input  [15:0] i_data_1,
+    input  [15:0] i_data_2,
+    output [15:0] o_data_1,
+    output [15:0] o_data_2,
+);
+
+endmodule
+
+module itp4 (
+    input  [15:0] i_data_1,
+    input  [15:0] i_data_2,
+    output [15:0] o_data_1,
+    output [15:0] o_data_2,
+    output [15:0] o_data_3,
+);
+
+endmodule
+
+module itp5 (
+    input  [15:0] i_data_1,
+    input  [15:0] i_data_2,
+    output [15:0] o_data_1,
+    output [15:0] o_data_2,
+    output [15:0] o_data_3,
+    output [15:0] o_data_4,
+);
+
+endmodule
+
+module itp6 (
+    input  [15:0] i_data_1,
+    input  [15:0] i_data_2,
+    output [15:0] o_data_1,
+    output [15:0] o_data_2,
+    output [15:0] o_data_3,
+    output [15:0] o_data_4,
+    output [15:0] o_data_5,
+);
+
+endmodule
+
+module itp7 (
+    input  [15:0] i_data_1,
+    input  [15:0] i_data_2,
+    output [15:0] o_data_1,
+    output [15:0] o_data_2,
+    output [15:0] o_data_3,
+    output [15:0] o_data_4,
+    output [15:0] o_data_5,
+    output [15:0] o_data_6,
+);
+
+endmodule
+
+module itp8 (
+    input  [15:0] i_data_1,
+    input  [15:0] i_data_2,
+    output [15:0] o_data_1,
+    output [15:0] o_data_2,
+    output [15:0] o_data_3,
+    output [15:0] o_data_4,
+    output [15:0] o_data_5,
+    output [15:0] o_data_6,
+    output [15:0] o_data_7,
+);
+
+endmodule
